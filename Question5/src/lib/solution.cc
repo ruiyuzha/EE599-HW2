69493433bfa71faf06ffa33950030780cfee357c
Ruiyu Zhao <ruiyuzha@usc.edu>
2020-02-03 21:55:45 -0800

    Update solution.cc

--- Question5/src/lib/solution.cc
+++ Question5/src/lib/solution.cc
@@ -1,6 +1,7 @@
 #include "solution.h"
 using namespace std;
 
+//O(1)
 string Solution::SwapChar(string input,int index1,int index2) { 
   char tmp;
   tmp = input[index1];
@@ -9,11 +10,13 @@ string Solution::SwapChar(string input,int index1,int index2) {
   return input; 
 }
 
+//O(n)
 string Solution::ReverseString(string input){
   reverse(input.begin(),input.end());
   return input;
 }
 
+//O(n)
 string Solution::LowerChar(string input){
   transform(input.begin(), input.end(), input.begin(), ::tolower);
   return input;

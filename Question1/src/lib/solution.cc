d597d3dac2d12fd0ec23af125102954d9a3d7877
Ruiyu Zhao <ruiyuzha@usc.edu>
2020-02-03 21:43:55 -0800

    Time Complexity

--- Question1/src/lib/solution.cc
+++ Question1/src/lib/solution.cc
@@ -3,14 +3,17 @@
 #include <string>
 using namespace std;
 
+//O(1)
 int Solution::Add(int input1,int input2){
     return input1+input2;
 }
 
+//O(1)
 string Solution::Add(string input1, string input2){
     return input1+input2;
 }
 
+//O(n)
 bool Solution::isNumber(string s) { 
     for (int i = 0; i < s.length(); i++) 
         if (isdigit(s[i]) == false) 

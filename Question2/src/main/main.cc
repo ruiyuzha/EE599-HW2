11255b227fbe57853523380a9a4bc23353903ff9
Ruiyu Zhao <ruiyuzha@usc.edu>
2020-02-03 21:52:32 -0800

    Update main.cc

--- Question2/src/main/main.cc
+++ Question2/src/main/main.cc
@@ -4,14 +4,17 @@ using namespace std;
 
 int main()
 {
+    //O(1)
     int a[]={1,2,3,4,5};
     //cout << "An array of integer is " << a << endl;
     cout << "Length of an array of integers = " << (sizeof(a)/sizeof(*a)) << endl;
 
+    //O(1)
     char b[]={'H','e','l','l','o'};
     //cout << "An array of char is " << b << endl;
     cout << "Length of an array of char = " << (sizeof(b)/sizeof(*b)) << endl;
 
+    //O(1)
     float c[]={3.14,2.68};
     //cout << "An array of float is " << *c << endl;
     cout << "Length of this an array of float is = " << (sizeof(c)/sizeof(*c)) << endl;
